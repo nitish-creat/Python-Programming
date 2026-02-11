09334dc481bda838ac57d34e82d743939ccc5521
nitish-creat <nitishrana2242006@gmail.com>
2026-02-11 11:45:59 +0530

    update panda

---
+++ PANDAS/basic.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Create a dictionary (data)
+data = {
+    "Name": ["Nitish", "Rahul", "Aman"],
+    "Age": [21, 22, 20],
+    "Marks": [85, 90, 78]
+}
+
+# Convert dictionary to DataFrame
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+print(df)
